c6abf82a496aafb30f6362b3f47cdee410fd469f
MrDollerman <muhammadanasbussiness@gmail.com>
2025-08-01 18:40:02 +0500

    update

--- fund me .sol
+++ fund me .sol
@@ -1,12 +1,35 @@
 // SPDX-License-Identifier: MIT 
  pragma solidity ^0.8.18;
+
+
  contract Fundme{
+uint256 minimumUsd = 5;
+
+
+
+
  function fund() public  payable  {
- require(msg.value >1e18, "dint send enough eth");
+ //Allow user to send $
+//   have a minimun $ of send $5
+// how do we send  ETH to this contract?
+
+
+ require(msg.value >minimumUsd, "dint send enough eth");
 
  }
 // function withdraw () public {}
 
+function getprice() public {
+// address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+// abi 
+
+}
+
+
+
+function getconvertionRate() public {
+
+}
 
 
  }  
