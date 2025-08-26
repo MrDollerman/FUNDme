8b0c29c11766637dc58da5ba38d40952d7bf4c4b
MrDollerman <muhammadanasbussiness@gmail.com>
2025-08-26 17:32:59 +0500

    take some functions to a library far a little convenient and make a for loop in withdraw funtions

--- fund me .sol
+++ fund me .sol
@@ -1,57 +1,35 @@
 // SPDX-License-Identifier: MIT 
  pragma solidity ^0.8.18;
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+import {PriceConverter} from "./PriceConverter.sol";
 
  contract Fundme{
+  using PriceConverter for uint256;
+
 uint256 public  minimumUsd = 5e18;
+
 address[] public funders;
+
 mapping (address funder => uint256 amountfunded) public AddressToAmountFunded;
+
     mapping(address => uint256) public contributionCount;
 
  function fund() public  payable  {
- //Allow user to send $
-//   have a minimun $ of send $5
-// how do we send  ETH to this contract?
 
-
- require(getconvertionRate(msg.value) >= minimumUsd, "dint send enough eth");
+require(msg.value.getconvertionRate() >= minimumUsd, "dint send enough eth");
 funders.push(msg.sender); 
-AddressToAmountFunded[msg.sender] = AddressToAmountFunded[msg.sender] + msg.value;
+AddressToAmountFunded[msg.sender] += msg.value;
 
 contributionCount[msg.sender] += 1;
  }
-// function withdraw () public {}
+
 
  function getContributionCount(address user) public view returns (uint256) {
         return contributionCount[user];
     }
 
-function getprice() public view returns (uint256) {
-// address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-// abi 
-AggregatorV3Interface pricefeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-   (,int answer,,,) =pricefeed.latestRoundData();
 
-// price in terms of usd 
-return uint256(answer* 1e10);
-
-}
- 
-
-
-function getconvertionRate(uint256 ETHamount) public view returns (uint256) {
-    // 1 ETH wirth ?
-
-    uint256 ethprice = getprice();
-    // (ETHprice* 1.000000000000000000)  / 1e18
-    // 1 ethprice in usd = 1 eth 
-  uint256  ETHamountinUSD = (ethprice *ETHamount) /1e18;
-  return ETHamountinUSD;
-}
-function getversion() public view returns (uint256 ) {
-return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
-
-}
 function decimalvalue() public view  returns (uint8) {
 return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).decimals();
 
@@ -60,16 +38,17 @@ function description () public view returns (string memory) {
 
   return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).description();
 }
-   function convertUsdToEth(uint256 usdAmount) public view returns(uint256){
-     uint256 ethPrice = getprice();
-    
-   uint256  scaledUSD = usdAmount*1e36;
-
-     uint256 ethamount = scaledUSD/ethPrice;
-    
-     return ethamount ;
-
+function convertUsdToEth(uint256 usdamount) public view returns (uint256 ) {
+return PriceConverter.convertUsdToEth(usdamount );
 
 }
+ function withdraw () public {
+  for (uint256 funderindex = 0; funderindex< funders.length ;funderindex++) 
+  {
+    address funder =funders[funderindex];
+    AddressToAmountFunded[funder] =0;
+  }
+
+ }
 
  }  
